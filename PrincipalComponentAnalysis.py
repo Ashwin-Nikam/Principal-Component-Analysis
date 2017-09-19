b22658bb55c2b5c842090a160ac6488404e85836
Ashwin Nikam <anikam@buffalo.edu>
2017-09-19 16:40:19 -0400

    calculating mean done

--- PrincipalComponentAnalysis.py
+++ PrincipalComponentAnalysis.py
@@ -2,7 +2,7 @@
 
 #First we find out the number of features
 
-file = open("../../Desktop/pca_a.txt", "r");
+file = open("../../Desktop/pca_c.txt", "r");
 lines = file.readlines();
 rows = len(lines);
 
@@ -14,13 +14,23 @@ def findNumFeatures(lines):   #Method for calculating the number of features
             nFeatures += 1;
     return nFeatures;
 
-def createMatrix():
+def createMatrix():           #Method for creating a feature matrix
     columns = findNumFeatures(lines);
     matrix = [[0 for x in range(columns)] for y in range(rows)];
-    for i in range(rows):
-        features = lines[i].split("\t");
-        for j in range(columns):
-            matrix[i][j] = features[j];
+    for row in range(rows):
+        features = lines[row].split("\t");
+        for column in range(columns):
+            matrix[row][column] = features[column];
+    findMean(matrix, rows, columns);
+
+def findMean(matrix, rows, columns):  #Calculating mean of each feature
+    mean = [0 for i in range(columns)];
+    for column in range(columns):
+        for row in range(rows):
+            mean[column] += float(matrix[row][column]);
+    print("Mean of all the features");
+    for number in mean:
+        number /= rows;
+        print(number);
 
 createMatrix();
-
