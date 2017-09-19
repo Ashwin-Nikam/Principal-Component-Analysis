c545e4289a05742ef26c6b99e6bb893d6d9718ce
Ashwin Nikam <anikam@buffalo.edu>
2017-09-19 16:23:52 -0400

    Create matrix

--- PrincipalComponentAnalysis.py
+++ PrincipalComponentAnalysis.py
@@ -1,21 +1,26 @@
-#Reading a file in python
-"""
-
-file = open("../../Desktop/pca_a.txt", "r");
-print(file.read());
-
-"""
-
 #Principal Component Analysis
 
 #First we find out the number of features
-file = open("../../Desktop/pca_a.txt", "r");
-lines = file.readlines();  #lines contains all the lines in the txt file
-record = lines[0];
-nFeatures = 0;
-for word in record:
-    if word == "\t":
-        nFeatures+=1;
 
-print(record);
-print("Number of features are ",nFeatures);
+file = open("../../Desktop/pca_a.txt", "r");
+lines = file.readlines();
+rows = len(lines);
+
+def findNumFeatures(lines):   #Method for calculating the number of features
+    record = lines[0];
+    nFeatures = 0;
+    for word in record:
+        if word == "\t":
+            nFeatures += 1;
+    return nFeatures;
+
+def createMatrix():
+    columns = findNumFeatures(lines);
+    matrix = [[0 for x in range(columns)] for y in range(rows)];
+    for i in range(rows):
+        features = lines[i].split("\t");
+        for j in range(columns):
+            matrix[i][j] = features[j];
+
+createMatrix();
+
