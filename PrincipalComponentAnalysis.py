7d59af1380ed337d06001dc99c10f0684addc69c
Ashwin Nikam <anikam@buffalo.edu>
2017-09-19 15:12:42 -0400

    Number of features

--- PrincipalComponentAnalysis.py
+++ PrincipalComponentAnalysis.py
@@ -7,3 +7,15 @@ print(file.read());
 """
 
 #Principal Component Analysis
+
+#First we find out the number of features
+file = open("../../Desktop/pca_a.txt", "r");
+lines = file.readlines();  #lines contains all the lines in the txt file
+record = lines[0];
+nFeatures = 0;
+for word in record:
+    if word == "\t":
+        nFeatures+=1;
+
+print(record);
+print("Number of features are ",nFeatures);
